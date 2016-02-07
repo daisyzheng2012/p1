ce9c8b94107a97ac91dcc0e3cc3cc2c7ef96081b
Jingjing Zheng <jingjingzheng@g.harvard.edu>
2016-02-07 13:54:15 -0500

    Add index file, and selfie

---
+++ index.php
@@ -0,0 +1,14 @@
+Name: Jingjing Zheng
+<img href="img/selfie.jpg">
+
+Project2 Title
+View:[link]
+Github:[link]
+
+Project3 Title
+View:[link]
+Github:[link]
+
+Project4 Title
+View:[link]
+Github:[link]
