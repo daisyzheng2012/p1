2b1c6a9a69d24447418227da39ee4deeb30f1411
Jingjing Zheng <jingjingzheng@g.harvard.edu>
2016-02-07 13:59:26 -0500

    Updated index.php

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
-Name: Jingjing Zheng
-<img href="img/selfie.jpg">
+<p1>Name: Jingjing Zheng</p1>
 
+<div>
+<img src="img/selfie.jpg">
+</div>
+
+<div>
 Project2 Title
 View:[link]
 Github:[link]
+</div>
 
+<div>
 Project3 Title
 View:[link]
 Github:[link]
+</div>
 
+<div>
 Project4 Title
 View:[link]
 Github:[link]
+</div>
